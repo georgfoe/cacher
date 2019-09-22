cbfee6232d9e7a2bf4bafc30fb5b0b58dee13bbc
Georg Förster <georgfoe@gmail.com>
2019-09-22 21:25:35 +0200

    Set dirs for build and cache files

--- R/cache.r
+++ R/cache.r
@@ -2,14 +2,17 @@ cache <- function(cache.name, reBuild = FALSE) {
 
   #attach(.GlobalEnv$.caches)
 
+  if (!dir.exists("caches")) dir.create("caches")
+  if (!dir.exists("build")) dir.create("build")
+
   if (!(".caches" %in% ls(envir = globalenv(), all.names = TRUE))) .caches <<- new.env(parent = globalenv())
 
   cache.name <- substitute(cache.name)
   if (class(cache.name) == "name") cache.name <- deparse(cache.name)
 
   cache.name.full = paste0(".GlobalEnv$.caches$", cache.name)
-  cache.file = paste0("data/cache/", cache.name, ".RDS")
-  build.file = paste0("R/build.data/", cache.name, ".r")
+  cache.file = paste0("caches/", cache.name, ".RDS")
+  build.file = paste0("build/", cache.name, ".r")
 
   if (exists(cache.name, envir = .GlobalEnv$.caches) & !(reBuild)) {
     message("## Cache '", cache.name, "' ist bereits vorhanden ##")
