febb6e9319c74e4d7098e5f438791b90e05d9fb6
Georg Förster <georgfoe@gmail.com>
2019-09-23 11:44:58 +0200

    Updating cache_dependencies for working with vectors of caches

--- R/cache_dependencies.r
+++ R/cache_dependencies.r
@@ -1,18 +1,22 @@
 #' Title
 #'
-#' @param cache.name
+#' @param cache.names
 #'
 #' @return
 #' @export
+#'
 #' @importFrom purrr map
 #' @importFrom magrittr %>%
 #' @importFrom stringr str_split
 #'
 #' @examples
-cache_dependencies <- function(cache.name) {
+cache_dependencies <- function(cache.names) {
 
-  cache.name <- substitute(cache.name)
-  if (class(cache.name) == "name") cache.name <- deparse(cache.name)
+  arg.name <- substitute(cache.names) %>% deparse()
+
+  if (arg.name %in% list_caches()) cache.names = arg.name
+
+  dep.list <- purrr::map(cache.names, function(cache.name) {
 
   build.file = paste0(cacher:::.get_build_dir_name(), "/", cache.name, ".r")
 
@@ -26,7 +30,18 @@ cache_dependencies <- function(cache.name) {
 
     str_split("\\)") %>%
     map(`[`, 1) %>%
+    gsub("\"", "", .) %>%
     unlist() %>%
-    unique()
+    unique() -> deps
+
+    if (length(deps) == 0) return(NULL) else return(deps)
+
+  })
+
+  names(dep.list) <- cache.names
+
+  if (length(dep.list) < 2) dep.list = dep.list[[1]]
+
+  return(dep.list)
 
 }
--- tests/testthat/test_cache_dependencies.r
+++ tests/testthat/test_cache_dependencies.r
@@ -32,3 +32,74 @@ test_that("Dependencies on other caches are retrieved correctly from build code"
             unlink(cacher:::.get_build_dir_name(), recursive = TRUE)
             unlink(cacher:::.get_cache_dir_name(), recursive = TRUE)
           })
+
+
+test_that("Parantheses are removed from cache-names",
+          {
+            clear_cache()
+
+            cacher:::.create_build_dir()
+            build.dir <- cacher:::.get_build_dir_name()
+
+            writeLines("test1 <- runif(100, 1, 1000)",
+                       paste0(build.dir, "/test1.r"))
+
+            writeLines(c("test2 <- {",
+                         "cache(\"test1\") ^ 2",
+                         "}"),
+                       paste0(build.dir, "/test2.r"))
+
+            writeLines(c("test3 <- {",
+                         "cache(test1) * cache(test2)",
+                         "}"),
+                       paste0(build.dir, "/test3.r"))
+
+            cache(test3) -> x
+
+            dep1 <- cache_dependencies(test1)
+            dep2 <- cache_dependencies(test2)
+            dep3 <- cache_dependencies(test3)
+
+            expect_equal(dep1, NULL)
+            expect_equal(dep2, c("test1"))
+            expect_equal(dep3, c("test1", "test2"))
+
+
+            unlink(cacher:::.get_build_dir_name(), recursive = TRUE)
+            unlink(cacher:::.get_cache_dir_name(), recursive = TRUE)
+          })
+
+
+test_that("cache_dependencies can be called with a character vector of caches and gives a list with dependencies back",
+          {
+            clear_cache()
+
+            cacher:::.create_build_dir()
+            build.dir <- cacher:::.get_build_dir_name()
+
+            writeLines("test1 <- runif(100, 1, 1000)",
+                       paste0(build.dir, "/test1.r"))
+
+            writeLines(c("test2 <- {",
+                         "cache(\"test1\") ^ 2",
+                         "}"),
+                       paste0(build.dir, "/test2.r"))
+
+            writeLines(c("test3 <- {",
+                         "cache(test1) * cache(test2)",
+                         "}"),
+                       paste0(build.dir, "/test3.r"))
+
+            cache(test3) -> x
+
+            deps <- cache_dependencies(c("test1", "test2", "test3"))
+
+            expect_type(deps, "list")
+            expect_named(deps, c("test1", "test2", "test3"))
+            expect_equal(deps$test1, NULL)
+            expect_equal(deps$test2, c("test1"))
+            expect_equal(deps$test3, c("test1", "test2"))
+
+            unlink(cacher:::.get_build_dir_name(), recursive = TRUE)
+            unlink(cacher:::.get_cache_dir_name(), recursive = TRUE)
+          })
