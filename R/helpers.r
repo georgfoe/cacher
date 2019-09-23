e54c003dee6eb872545d4b408d0a24ae4cf41151
Georg Förster <georgfoe@gmail.com>
2019-09-23 08:15:47 +0200

    Create helper functions for creating build dir

---
+++ R/helpers.r
@@ -0,0 +1,30 @@
+# helper Functions
+
+#' Title
+#'
+#' @return
+#'
+#' @examples
+.create_build_dir <- function() {
+
+  build.dir.name <- .get_build_dir_name()
+
+  if (!dir.exists(build.dir.name)) {
+    dir.create(build.dir.name)
+  }
+}
+
+#' Title
+#'
+#' @return
+#'
+#' @examples
+.get_build_dir_name <- function() {
+  default <- getOption("build.dir")
+  if (!is.null(default)) {
+    return(default)
+  } else {
+    return("build")
+  }
+}
+
